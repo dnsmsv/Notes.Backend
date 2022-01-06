36db0905b2add4221b26af85bdb0c16cdf8f7a1a
dnsmsv <dnsmsv@gmail.com>
2022-01-06 14:58:26 +0300

    Added database.

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<Note> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        Task<Note> INotesDbContext.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
