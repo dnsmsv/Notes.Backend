0f6016993127dac1bd8fe141392033aed76d4247
dnsmsv <dnsmsv@gmail.com>
2022-01-11 15:41:03 +0300

    Custom middleware for handle exceptions.

---
+++ Notes.WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Notes.Application.Common.Exceptions;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Notes.WebApi.Middleware
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next) =>
+            this.next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
--- Notes.WebApi/Startup.cs
+++ Notes.WebApi/Startup.cs
@@ -7,6 +7,7 @@ using Notes.Application;
 using Notes.Application.Common.Mappings;
 using Notes.Application.Interfaces;
 using Notes.Persistence;
+using Notes.WebApi.Middleware;
 using System.Reflection;
 
 namespace Notes.WebApi
@@ -47,6 +48,7 @@ namespace Notes.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCustomExceptionHandler();
             app.UseRouting();
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
